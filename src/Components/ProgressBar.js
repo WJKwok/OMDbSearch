af8062540e958967a440e0cd9e6c811f88101283
Wen <kwok.wenjian@gmail.com>
2021-01-07 14:15:18 +0100

    error msg when nomination exceeds limit

--- src/Components/ProgressBar.js
+++ src/Components/ProgressBar.js
@@ -6,6 +6,9 @@ import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
 	root: {
+		marginBottom: 10,
+	},
+	progressDiv: {
 		display: 'flex',
 		alignItems: 'center',
 	},
@@ -20,22 +23,30 @@ const useStyles = makeStyles({
 export const ProgressBar = ({ goal, current }) => {
 	const classes = useStyles();
 
-	const progress = () => {
-		const progressPercentage = (current / goal) * 100;
-		return progressPercentage;
-	};
+	const exceededGoal = current > goal;
+	const progress = exceededGoal ? 100 : (current / goal) * 100;
 
 	return (
 		<div className={classes.root}>
-			<LinearProgress
-				className={classes.progressBar}
-				variant="determinate"
-				value={progress()}
-			/>
-			<Typography className={classes.progressNumber} variant="body1">
-				{current}/{goal}
-			</Typography>
-			<Button disabled={current !== goal}>Nominate</Button>
+			<div className={classes.progressDiv}>
+				<LinearProgress
+					className={classes.progressBar}
+					variant="determinate"
+					color={exceededGoal ? 'secondary' : 'primary'}
+					value={progress}
+				/>
+				<Typography className={classes.progressNumber} variant="body1">
+					{current}/{goal}
+				</Typography>
+				<Button variant="outlined" disabled={current !== goal}>
+					Nominate
+				</Button>
+			</div>
+			{exceededGoal && (
+				<Typography variant="body1" color="secondary">
+					You can only nominate 5 movies, please remove {current - goal}
+				</Typography>
+			)}
 		</div>
 	);
 };
