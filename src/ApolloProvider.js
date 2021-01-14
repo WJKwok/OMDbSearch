4e5a815c44565e471e356dc837f5506fc387a41e
Wen <kwok.wenjian@gmail.com>
2021-01-14 15:00:43 +0100

    added heroku backend uri

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -3,7 +3,10 @@ import App from './App';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-	uri: 'http://localhost:4000/',
+	uri:
+		process.env.NODE_ENV === 'production'
+			? 'https://shopify-code-challenge-backend.herokuapp.com/'
+			: 'http://localhost:4000/',
 	cache: new InMemoryCache(),
 });
 
